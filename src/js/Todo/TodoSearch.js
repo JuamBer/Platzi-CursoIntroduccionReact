f748807a796f2cb1b09c672749284cfbc29ea004
Juan Sáez García <contacto@juamber.com>
2022-01-12 17:47:11 +0100

    estados

--- src/js/Todo/TodoSearch.js
+++ src/js/Todo/TodoSearch.js
@@ -2,17 +2,23 @@ import React from "react";
 import '../../css/Todo/TodoSearch.css';
 
 function TodoSearch() {
+    const [searchValue, setSearchValue] = React.useState('');
 
     const onSearchValueChange = (event)=>{
         console.log(event.target.value);
+        setSearchValue(event.target.value);
     }
 
     return (
-        <input 
-            className="TodoSearch" 
-            placeholder="Cebolla"
-            onChange={onSearchValueChange}
-        />
+        <div>
+            <input 
+                className="TodoSearch" 
+                placeholder="Cebolla"
+                value={searchValue}
+                onChange={onSearchValueChange}
+            />
+            <p>{searchValue}</p>
+        </div>
     );
 }
 
